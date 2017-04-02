8ae40c96a48ff412174f647e567bb3553c1b7f86
hadoop <hadoop@ubuntu>
2017-04-02 04:56:11 -0700

    init project

---
+++ src/main/java/com/xmu/hadoop/aco/acovrp/App.java
@@ -0,0 +1,13 @@
+package com.xmu.hadoop.aco.acovrp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
